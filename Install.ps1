5d7fd7d0ec1a7a6fa7b469471b73e600b418c56a
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2022-06-14 21:37:21 -0400

    Add comments

--- Install.ps1
+++ Install.ps1
@@ -7,6 +7,7 @@ Unblock-File $PSScriptRoot\Lang-*.ps1
 Unblock-File $PSScriptRoot\Get-TeamsStatus.ps1
 
 # In case this is re-run as a reinstall
+# If you get a "Cannot find any service with service name" do not worry about it as it just means the service was not installed yet
 UninstallService
 
 # Enable logging if active
@@ -14,6 +15,7 @@ if ($enableLogs -eq "Y") {
     $loggingParam = "2>&1 | tee -filePath $PSScriptRoot\TeamsStatusLog.txt"
 }
 
-$argumentList = 'install "Microsoft Teams Status Monitor" "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" "-command "& { . ' + "$PSScriptRoot\Get-TeamsStatus.ps1 $loggingParam" + '}"" '
+$argumentList = 'install "Microsoft Teams Status Monitor" "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" "-command "& { . ' + 
+    "$PSScriptRoot\Get-TeamsStatus.ps1 $loggingParam" + '}"" '
 Start-Process -FilePath $PSScriptRoot\nssm.exe -ArgumentList $argumentList -NoNewWindow -Wait
 Start-Service -Name "Microsoft Teams Status Monitor"
